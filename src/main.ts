79aae60620d750d0f66394fd687b195900772ad5
p32929 <p32929@yahoo.com>
2023-09-04 22:57:21 +0600

    updated tests

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,17 @@
-import { StrTemplate } from "./../lib/index"
+import { StrTemplate } from ".";
 
-const yourString = `Hello [[name1]], How is [[name2]]?`
-const finalString = StrTemplate.format(yourString, {
-    name1: "Fayaz",
-    name2: "Farhaz"
-}, "[[]]")
-console.log(finalString) // Hello Fayaz, How is Farhaz?
+// const formattedString = StrTemplate.format("Hello world {HHHH}", {
+//     "HHH": "HAHAHAH"
+// })
+
+// console.log(``)
+
+// const aa = StrTemplate.splitStringInMiddle("[[]]")
+// console.log(``)
+
+// const yourString = `Hello [[name1]], How is [[name2]]?`
+// const finalString = StrTemplate.format(yourString, {
+//     name1: "Fayaz",
+//     name2: "Farhaz"
+// }, "[[]]")
+// console.log(finalString) // Hello Fayaz, How is Farhaz?
